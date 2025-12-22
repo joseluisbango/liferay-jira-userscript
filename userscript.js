206a2eb7787343fa84066311e0ebf16ef473d69f
Daniel Martínez Cisneros <daniel.martinez@liferay.com>
2025-12-22 09:57:19 +0100

    fixed Text is black on a black background when switching from Internal Comment to Public

--- userscript.js
+++ userscript.js
@@ -3,7 +3,7 @@
 // @author       Ally, Rita, Dmcisneros
 // @icon         https://www.liferay.com/o/classic-theme/images/favicon.ico
 // @namespace    https://liferay.atlassian.net/
-// @version      3.8
+// @version      3.9
 // @description  Jira statuses + Patcher, Account tickets and CP Link field + Internal Note highlight + Auto Expand CCC Info
 // @match        https://liferay.atlassian.net/*
 // @match        https://liferay-sandbox-424.atlassian.net/*
